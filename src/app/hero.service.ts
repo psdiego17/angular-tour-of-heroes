4ac9fb1f367c06f160bfc3edd735f6725e70a632
Diêgo Souza <psdiego17@gmail.com>
2021-07-30 16:03:34 -0300

    adicionado a parte de serviços ao projeto

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,18 +2,22 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+  constructor(private messageService: MessageService) { }
  
+  //método para enviar uma mensagem quando os heróis forem buscados.
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
   
-  constructor() { }
 }
 
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,49 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';//Fazendo o import do hero.ts
-//import { HEROES } from '../mock-heroes'; //Fazendo o import do mock-heroes.ts que está atuando como um banco de dados
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
-/*Sempre exporta classe de componente para que você possa em import outro lugar ... 
-como no AppModule. */
-
 export class HeroesComponent implements OnInit {
-  
-  heroes: Hero[] = [];
-  selectedHero?: Hero;
-  /*Refatorando a hero propriedade do componente para ser do tipo Hero. 
-  Inicializando com um id de 1 e o nome Windstorm. */
-  
-  /*
-  hero: Hero = { //Criação do objeto hero do tipo Hero que é uma interface
-    id: 1,
-    name: 'Windstorm'
-  }*/
-  
-  onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
-  
-  constructor(
-    
-    private heroService: HeroService
-  ) { }
 
-  /*O ngOnInit()é um gancho de ciclo de vida . 
-  Chamadas angulares ngOnInit()logo após a criação de um componente. 
-  É um bom lugar para colocar a lógica de inicialização.*/
+  selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
   ngOnInit() {
     this.getHeroes();
   }
-  
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
