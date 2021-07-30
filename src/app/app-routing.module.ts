05019e64ee733004ea505a65e30ecd4424df52a6
Diêgo Souza <psdiego17@gmail.com>
2021-07-30 16:20:04 -0300

    adicionado nav para navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-
+/*onfigura suas rotas. As rotas informam ao roteador qual visualização exibir quando 
+um usuário clica em um link ou cola uma URL na barra de endereço do navegador. 
+path: uma string que corresponde ao URL na barra de endereço do navegador.
+component: o componente que o roteador deve criar ao navegar para esta rota.
+*/
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
