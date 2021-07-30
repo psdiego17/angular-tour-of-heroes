7f24c21c9e05242cd3508f917d1507417866db77
Diêgo Souza <psdiego17@gmail.com>
2021-07-30 14:56:02 -0300

    criado o component hero-detail e feito o desmembramento do arquivo html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
-@NgModule({ //matriz onde são declarados as informações dos imports
+@NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
